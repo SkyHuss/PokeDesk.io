cd4f9b1ea7df397843e9a50ab33cbdf45fedfc9d
SkyHuss <fl.gonzales5790@gmail.com>
2025-02-26 18:39:52 +0100

    feat(router): implement sidebar and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
 import "./App.css"
+import Sidebar from "./components/sidebar/Sidebar"
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import "./contants/color.css"
+import Inventory from "./pages/inventory/Inventory";
+import Pokedex from "./pages/pokedex/Pokedex";
+import Shop from "./pages/shop/Shop";
+import UserProfile from "./pages/userProfile/UserProfile";
+import Catch from "./pages/catch/Catch";
+
+function AppLayout() {
+  return (
+    <div id='app-container' className='app-container'>
+      <Sidebar />
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [ // Children are render in the Outlet
+      {
+        path: '/catch',
+        element: <Catch />
+      },
+      {
+        path: '/inventory',
+        element: <Inventory />
+      },
+      {
+        path: '/pokedex',
+        element: <Pokedex />
+      },
+      {
+        path: '/store',
+        element: <Shop />
+      },
+      {
+        path: '/account',
+        element: <UserProfile />
+      }
+    ]
+  }
+])
 
 export default function App() {
-  return <div className="app-container">
-    Hello world
-  </div>
+
+  return (
+    <RouterProvider router={router} />
+  )
+
 }
